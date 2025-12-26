9ed952e4dcfcb1d1d0e419932dba839db5ac3166
rudra <bhattacharjeerudra947@gmail.com>
2025-12-27 00:58:11 +0530

    giving sound effect

--- snake_game/snake.cpp
+++ snake_game/snake.cpp
@@ -2,8 +2,7 @@
 #include<conio.h>
 #include<windows.h>
 #include <ctime>
-
-
+#pragma comment(lib, "winmm.lib")
 void showConsoleCursor(bool showFlag) {
     HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO cursorInfo;
@@ -11,12 +10,18 @@ void showConsoleCursor(bool showFlag) {
     cursorInfo.bVisible = showFlag;   // true = visible, false = hide
     SetConsoleCursorInfo(out, &cursorInfo);
 }
-
-
 using namespace std;
 const int UI_HEIGHT = 3;
-
-
+void play_bgm(){
+    PlaySound(
+        TEXT("C:\\Users\\bhatt\OneDrive\Desktop\c++ project\c-projects\snake_game"),        // play this file
+        NULL,               // it is not inside program
+        SND_FILENAME |         // it's a file
+        SND_ASYNC |            // play in background
+        SND_LOOP               // keep repeating
+    );
+    
+}
 #define MAX_LENGTH 1000
 //DIRECTION
 const char d_up= 'w';
@@ -30,6 +35,29 @@ void initscreen(){
     GetConsoleScreenBufferInfo(hConsole,&csbi);
     con_h=csbi.srWindow.Bottom-csbi.srWindow.Top+1;
     con_w=csbi.srWindow.Right-csbi.srWindow.Left+1;
+}
+void play_eat(){
+    PlaySound(
+        TEXT("mixkit-winning-a-coin-video-game-2069.wav"),
+        nullptr,
+        SND_FILENAME | SND_ASYNC
+    );
+    Sleep(150);
+    play_bgm();
+
+}
+void play_bonus(){
+    PlaySound(TEXT("mixkit-game-bonus-reached-2065.wav"),nullptr,SND_FILENAME | SND_ASYNC);
+    play_bgm();
+}
+void play_crash(){
+    PlaySound(
+        TEXT("mixkit-player-losing-or-failing-2042.wav"),
+        nullptr,
+        SND_FILENAME | SND_ASYNC
+
+    );
+    play_bgm();
 }
 struct point{
     int xcoord;
@@ -52,8 +80,7 @@ class Snake{
             length=1;
             body[0]=point(x,y);
             direction =d_right;
-        }
-   
+        }  
     int getLength(){
         return length;
     }
@@ -78,7 +105,6 @@ class Snake{
         for(int i = length - 1; i > 0; i--){
         body[i] = body[i - 1];
         }
-
         switch(direction){
             case d_up:    body[0].ycoord--; break;
             case d_down:  body[0].ycoord++; break;
@@ -86,27 +112,27 @@ class Snake{
             case d_left:  body[0].xcoord--; break;
         }
          // 👉 WALL COLLISION (HERE)
-        if(body[0].ycoord < UI_HEIGHT || body[0].ycoord >= con_h) return false;
-
+        if(body[0].ycoord < UI_HEIGHT || body[0].ycoord >= con_h){
+            play_crash();
+            return false;
+        } 
         // self collision
         for(int i = 1; i < length; i++){
-            if(body[0].xcoord == body[i].xcoord && body[0].ycoord == body[i].ycoord) return false;
+            if(body[0].xcoord == body[i].xcoord && body[0].ycoord == body[i].ycoord){
+                play_crash();
+                return false;
+            }
         }
-        
-
         if(food.xcoord == body[0].xcoord && food.ycoord == body[0].ycoord){
             body[length] = prevTail;
             length++;
         }
-
     return true;
     }
-
-
 };
 class Board{
+    bool bonusSoundPlayed=false;
     const int BAR_WIDTH = 20;
-
     Snake *snake;
     const char SNAKE_BODY = 'X';
     //point smfood;
@@ -128,28 +154,20 @@ class Board{
         for(int x = 0; x < con_w; x++){
             gotoxy(x, 0); cout << "-";
         }
-
     // middle content
         gotoxy(1,1);
         cout << "score: " << temp_score;
-
         drawBoostBar();
-
     // bottom border
         for(int x = 0; x < con_w; x++){
             gotoxy(x, UI_HEIGHT - 1); cout << "-";
         }
     }
-
-
     void drawBoostBar(){
         gotoxy(con_w - BAR_WIDTH - 12, 1);
    // below score line
-
         cout << "BOOST: [";
-
         double percent = 1.0;   // 0 → empty, 1 → full
-
     // If boost is active → bar decreases
         if(speedboost){
             double elapsed =
@@ -164,14 +182,11 @@ class Board{
             percent = elapsed / cooldown_time;
             if(percent > 1) percent = 1;
         }
-
         int filled = int(percent * BAR_WIDTH);
-
         for(int i = 0; i < BAR_WIDTH; i++){
             if(i < filled) cout << char(219);   // █ block
             else cout << " ";
         }
-
         cout << "]";
     }
     void gotoxy(int x,int y){
@@ -179,17 +194,12 @@ class Board{
         coord.X=x;
         coord.Y=y;
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
-
     }
-
-    
     Board(){
        snake = new Snake(10, UI_HEIGHT + 2);
-;
         temp_score = 0;
         speedboost = false;
         cooldown=false;
-
     }
     ~Board(){
         delete snake;
@@ -201,16 +211,13 @@ class Board{
         while(true){
             int x = rand() % (con_w - 2);
             int y = rand() % (con_h - UI_HEIGHT - 1) + UI_HEIGHT;
-
             bool onSnake = false;
-
             for(int i = 0; i < snake->getLength(); i++){
                 if(snake->body[i].xcoord == x && snake->body[i].ycoord == y){
                     onSnake = true;
                     break;
                 }
             }
-
             if(!onSnake){
                 food = point(x,y);
                 break;
@@ -218,29 +225,29 @@ class Board{
         }
     }
     int getCurrentSpeed(){
-
     // BOOST ACTIVE
         if(speedboost){
+            if(!bonusSoundPlayed){
+                play_bonus();
+                bonusSoundPlayed=true;
+            }
             double boostElapsed = double(clock() - boostStartTime) / CLOCKS_PER_SEC;
-
             if(boostElapsed >= boost_time){
                 speedboost = false;
                 cooldown = true;
                 cooldownStartTime = clock();   // start cooldown
+                bonusSoundPlayed=false;
                 return normal_speed;
             }
             return boost_speed;
         }
-
-    // COOLDOWN ACTIVE
+   // COOLDOWN ACTIVE
         if(cooldown){
             double cooldownElapsed = double(clock() - cooldownStartTime) / CLOCKS_PER_SEC;
-
             if(cooldownElapsed >= cooldown_time){
                 cooldown = false;  // boost available again
             }
         }
-
         return normal_speed;
     }
     void displaycurr_score(){
@@ -248,57 +255,54 @@ class Board{
         cout<<"score" <<temp_score;
     }
     pair<int,string> highest_score(int temp_score){
-        int highest = 0;
+        int highest=0 ;
         string name, oldName;
-
-    // 1️⃣ Read old highest score and name
-        ifstream fin("score.txt");
-        if (fin >> highest >> oldName) {
-        // read successful
-        }
-        fin.close();
-
+    // 1️⃣ Read old highest score and name  
         // 2️⃣ If new score is higher
-        if (highest==0 || temp_score > highest) {
-            
+        if ( temp_score > highest) {
+            int x=50;
+            int y= 4;
+            gotoxy(x,y);
+            cout<<"enter your name : ";
             cin >> name;
-
             ofstream fout("score.txt");
             fout << temp_score << " " << name;
             fout.close();
+            highest= temp_score;
         }
         else{
             name=oldName;
         }
+        ifstream fin("score.txt");
+        if (fin >> highest >> oldName) {
+        // read successful
+        }
+        fin.close();    
         return {highest,name};
     }
     void draw(){
         system("cls");
-
     // 🔵 DRAW TOP UI BOX
         drawUI();
-
     // 🟢 DRAW SNAKE (PLAY AREA ONLY)
         for(int i = 0; i < snake->getLength(); i++){
             gotoxy(snake->body[i].xcoord, snake->body[i].ycoord);
             cout << SNAKE_BODY;
         }
-
     // 🔴 DRAW FOOD
         gotoxy(food.xcoord, food.ycoord);
         cout << FOOD;
     }
-
     bool update(){
          bool isAlive = snake->move(food);
        if(isAlive == false)
        {
            return false;
        }
-
         if(food.xcoord == snake->body[0].xcoord && food.ycoord == snake->body[0].ycoord)
         {
             temp_score++;
+            play_eat();
             spawnFood();
         }
        return true;
@@ -330,7 +334,6 @@ class Board{
 
                 }
             }
-      
         }
     }
     void showGameOver(){
@@ -339,52 +342,43 @@ class Board{
         pair<int,string> high=highest_score(temp_score);
         string name=high.second;
         int hs=high.first;
-
     // Clear screen
         system("cls");
-
     // Set bold red color
         SetConsoleTextAttribute(
             hConsole,
             FOREGROUND_RED | FOREGROUND_INTENSITY
         );
         string h_score;
-
         string msg = "GAME OVER";
         string scoreMsg = "FINAL SCORE : " + to_string(temp_score);
-        if(temp_score==hs){
-
-            h_score="highes score is : " + to_string(hs) + "it's you  ("+name + " )";
+        if(temp_score>hs){
+            h_score="highes score is : " + to_string(hs) + "  it's you  ("+name + " )";
 
         }
         else  h_score="highes score is : " + to_string(hs)+" by "+"( "+name + " )";
         string highStr = h_score;
-
         int x1 = (con_w - msg.length()) / 2;
         int y1 = con_h / 2;
-
         int x2 = (con_w - scoreMsg.length()) / 2;
         int y2 = y1 + 2;
         int x3 =(con_w-highStr.length())/2;
         int y3 = y2+2;
-
         gotoxy(x1, y1);
         cout << msg;
-
         gotoxy(x2, y2);
         cout << scoreMsg<<"   "<<endl;
         gotoxy(x3,y3);
         cout<< h_score<<  " "<<endl;
-
     // Reset color back to normal
         SetConsoleTextAttribute(
             hConsole,
             FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE
         );
     }
-
 };
 int main(){
+    play_bgm();
     showConsoleCursor(false);
     srand(time(0));
     initscreen();
@@ -401,7 +395,4 @@ int main(){
     }
     delete board;   // ✅ free memory
     return 0;
-    exit;
-
-    
 }
